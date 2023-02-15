9dc0993b5703031c8ab8dbfb60f458a07fcb9dc0
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-02-15 17:16:43 +0100

    chore: Adds missing chakra dependencies

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react'
 import { Button, Heading, HStack, VStack } from '@chakra-ui/react'
 // @ts-ignore
-import ExampleForm, { ExampleFormFields } from '../ExampleForm'
+import ExampleForm, { ExampleFormFields } from './ExampleForm'
 
 const AppComponent: FC = () => {
   const [formSize, setFormSize] = useState<string>('sm')
