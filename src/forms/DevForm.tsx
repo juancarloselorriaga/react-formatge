3f38128651b6b60b4a6fe792bcf98857375e8046
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-02-25 19:58:16 +0100

    fix: Removes buttonprops from DevForm.tsx

--- src/forms/DevForm.tsx
+++ src/forms/DevForm.tsx
@@ -1,6 +1,6 @@
-import { Button, HStack, StackProps, VStack } from '@chakra-ui/react'
-import { FormFieldType, FormUpdatePayload, OnFormSubmit } from '../types'
-import React, { FC, useState } from 'react'
+import { StackProps } from '@chakra-ui/react'
+import { FormFieldType, OnFormSubmit } from '../types'
+import React, { FC } from 'react'
 import FormWrapper from '../_layout/FormWrapper'
 
 export type DevFormFields = {
@@ -32,33 +32,12 @@ const DevFormComponent: FC<DevFormComponentProps> = ( { data, buttonLabel, onFor
     await onFormSubmit( updatedData )
   }
 
-  const buttonProps = {
-    display: 'none',
-  }
-
-  const [ formUpdate, setFormUpdate ] = useState<FormUpdatePayload<DevFormFields> | null>( null )
-
   return (
-    <VStack { ...props }>
-      <FormWrapper<DevFormFields>
-        onSubmitCb={ handleOnFormSubmit }
-        onUpdate={ setFormUpdate }
-        { ...{ inputFields, buttonProps } }
-      />
-      <HStack w={ 'full' } justify={ 'flex-end' }>
-        <Button
-          colorScheme={ 'red' }
-          variant={ 'ghost' }>
-          Cancel
-        </Button>
-        <Button
-          isDisabled={ !formUpdate?.isEnabled }
-          onClick={ () => formUpdate && handleOnFormSubmit( formUpdate.updatedData ) }
-        >
-          Accept
-        </Button>
-      </HStack>
-    </VStack>
+    <FormWrapper<DevFormFields>
+      onSubmitCb={ handleOnFormSubmit }
+      { ...{ inputFields } }
+      { ...props }
+    />
   )
 }
 
