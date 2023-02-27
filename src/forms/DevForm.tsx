26d0475de0321bed0e5566f103728691a03b958d
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-02-27 22:02:32 +0100

    update: Updates dev form

--- src/forms/DevForm.tsx
+++ src/forms/DevForm.tsx
@@ -1,10 +1,11 @@
 import { StackProps } from '@chakra-ui/react'
 import { FormFieldType, OnFormSubmit } from '../types'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import FormWrapper from '../_layout/FormWrapper'
 
 export type DevFormFields = {
   field: string
+  otherField: string
 }
 
 interface DevFormComponentProps extends StackProps {
@@ -15,16 +16,32 @@ interface DevFormComponentProps extends StackProps {
 }
 
 const DevFormComponent: FC<DevFormComponentProps> = ( { data, buttonLabel, onFormSubmit, ...props } ) => {
+  const [ variable, setVariable ] = useState( '',
+  )
   const inputFields: FormFieldType<DevFormFields>[] = [
     {
       componentType: 'input',
       name: 'field',
       label: 'field',
+      onValueChange: ( payload ) => setVariable( payload.value ),
       initialValue: data?.field || '',
       validation: {
         required: true,
       },
     },
+    ...(
+      variable === 'test' ? [
+        {
+          componentType: 'input' as const,
+          name: 'otherField' as const,
+          label: 'other field',
+          initialValue: data?.otherField || '',
+          validation: {
+            required: true,
+          },
+        },
+      ] : []
+    ),
   ]
 
   const handleOnFormSubmit = async ( updatedData: Partial<DevFormFields> ) => {
