8acadcead5bd178683eb5605f703e66dbf45037d
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-02-28 14:37:23 +0100

    feat: Adds Option Selection Component

---
+++ src/_components/OptionSelection/OptionSelection.tsx
@@ -0,0 +1,38 @@
+import { Select, VStack, SelectProps, StackProps } from '@chakra-ui/react'
+import React, { FC } from 'react'
+
+export interface IOption {
+  label: string
+  value: string | null
+  color?: string
+}
+
+export interface OptionSelectionProps extends SelectProps {
+  options: IOption[]
+  label?: string
+  alignItems?: string
+  wrapperProps?: StackProps
+}
+
+const OptionSelection: FC<OptionSelectionProps> = ( { options, label, alignItems, wrapperProps, ...selectProps } ) => {
+  return (
+    <VStack
+      alignItems={ alignItems }
+      w='100%' { ...wrapperProps }>
+      <Select
+        variant='filled'
+        { ...selectProps }>
+        { options?.map( ( { value, label } ) => (
+          <option
+            key={ value }
+            value={ value ?? '' }
+          >
+            { label }
+          </option>
+        ) ) }
+      </Select>
+    </VStack>
+  )
+}
+
+export default OptionSelection
