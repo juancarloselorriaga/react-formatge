727ffbd99ae2b5aebe80dc0e97f1d21802373458
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-02-28 14:40:43 +0100

    chore: Updates dev form

--- src/forms/DevForm.tsx
+++ src/forms/DevForm.tsx
@@ -2,11 +2,10 @@ import { StackProps } from '@chakra-ui/react'
 import { FormFieldType, OnFormSubmit } from '../types'
 import React, { FC } from 'react'
 import FormWrapper from '../_layout/FormWrapper'
-import { formValidationRgx } from '../helpers/rgx'
+import OptionSelectionComponent from '../_components/OptionSelection/OptionSelectionComponent'
 
 export type DevFormFields = {
-  email: string
-  password: string
+  optionSelection: string
 }
 
 interface DevFormComponentProps extends StackProps {
@@ -19,35 +18,18 @@ interface DevFormComponentProps extends StackProps {
 const DevFormComponent: FC<DevFormComponentProps> = ( { data, buttonLabel, onFormSubmit, ...props } ) => {
 
   const inputFields: FormFieldType<DevFormFields>[] = [
-   {
-      componentType: 'input',
-      name: 'email',
-      label: 'email',
-      placeholder: 'type the email',
-      helperText: 'Login mail',
-      initialValue: '',
-      validation: {
-        required: true,
-        validator: {
-          regEx: formValidationRgx.email,
-          error: 'Invalid email format'
-        }
-      }
-    },
     {
-      componentType: 'input',
-      name: 'password',
-      label: 'password',
-      type: 'password',
-      initialValue: '',
-      validation: {
-        required: true,
-        validator: {
-          regEx: formValidationRgx.limitChars(4, 25),
-          error: 'The password must be 4 valid character long'
-        }
-      }
-    },
+      componentType: 'component',
+      name: 'optionSelection',
+      label: 'option selection',
+      initialValue: 'unselected',
+      component: (
+        <OptionSelectionComponent items={[
+          {label: 'Unselected', value: 'unselected'},
+          {label: 'Selected', value: 'selected'}
+        ]} />
+      )
+    }
   ]
 
   const handleOnFormSubmit = async ( updatedData: Partial<DevFormFields> ) => {
