b50002e7ecd911c90b31eac0e79bb268574901e0
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-04-05 18:25:28 +0200

    fix: Adds Multiple Selection component to index

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import FormWrapper from './_layout/FormWrapper'
 import { formValidationRgx } from './helpers/rgx'
 import { slugify, uppercasefy, formatDate, titleCasify } from './helpers/utils'
 import RadioButtonsComponent from './_components/RadioButtonsComponent'
+import MultipleOptionSelectionComponent from './_components/OptionSelection/MultipleOptionSelectionComponent'
 
 export {
   DatePickerComponent,
@@ -14,6 +15,7 @@ export {
   CheckboxComponent,
   OptionSelectionComponent,
   RadioButtonsComponent,
+  MultipleOptionSelectionComponent,
   FormWrapper,
   FormItemWrapper,
   formValidationRgx,
