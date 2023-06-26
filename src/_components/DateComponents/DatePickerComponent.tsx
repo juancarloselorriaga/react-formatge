c2f0ec3956ef9090e04999aa481282ec42eb1b5c
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-06-26 15:04:02 +0200

    fix: DataPickerComponent accepts null values

--- src/_components/DateComponents/DatePickerComponent.tsx
+++ src/_components/DateComponents/DatePickerComponent.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useMemo } from 'react'
 import { Calendar } from 'react-date-range'
 import { formatDate } from '../../helpers/utils'
 import DateComponentWrapper from './DateComponentWrapper'
@@ -6,27 +6,41 @@ import { CustomComponentImplementation } from '../../types'
 
 interface DatePickerComponentProps extends CustomComponentImplementation<Date> {
   title: string
-  onChange?: (date: Date) => void
+  onChange?: ( date: Date ) => void
   dateDisplayFormat?: string
 }
 
-const DatePickerComponent: FC<DatePickerComponentProps> = ({
-  title,
-  dateDisplayFormat = 'yyyy-MM-dd',
-  onChange,
-  defaultValue = new Date(),
-  onUpdateValue,
-  value,
-  ...props
-}) => {
-  const updateDate = (date: Date) => {
-    onChange && onChange(date)
-    onUpdateValue && onUpdateValue(date)
+const DatePickerComponent: FC<DatePickerComponentProps> = (
+  {
+    title,
+    dateDisplayFormat = 'yyyy-MM-dd',
+    onChange,
+    defaultValue,
+    onUpdateValue,
+    value,
+    ...props
+  } ) => {
+  const updateDate = ( date: Date ) => {
+    onChange && onChange( date )
+    onUpdateValue && onUpdateValue( date )
   }
 
+  const date = useMemo( () => {
+    if ( value ) {
+      return `${ formatDate( value ) }`
+    } else if ( defaultValue ) {
+      return `${ formatDate( defaultValue ) }`
+    } else {
+      return '-'
+    }
+  }, [value, defaultValue] )
+
   return (
-    <DateComponentWrapper value={`${formatDate(value || defaultValue)}`} {...{ title }} {...props}>
-      <Calendar editableDateInputs={true} date={value} onChange={updateDate} {...{ dateDisplayFormat }} />
+    <DateComponentWrapper value={ date } { ...{ title } } { ...props }>
+      <Calendar
+        editableDateInputs={ true }
+        date={ value }
+        onChange={ updateDate } { ...{ dateDisplayFormat } } />
     </DateComponentWrapper>
   )
 }
