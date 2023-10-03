e92dad08671551b18108e59eba8ebb77a2f2d532
Juan Carlos Elorriaga <juancarlos@jjaas.mx>
2023-10-03 14:10:20 +0200

    chore: Package exposes internal components

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,26 @@ import { formValidationRgx } from './helpers/rgx'
 import { slugify, uppercasefy, formatDate, titleCasify, parseInitialValue } from './helpers/utils'
 import RadioButtonsComponent from './_components/RadioButtonsComponent'
 import MultipleOptionSelectionComponent from './_components/OptionSelection/MultipleOptionSelectionComponent'
+import InputField from './_components/InputField'
+import TextComponent from './_components/TextComponent'
+import PopoverComponent from './_components/PopOver'
+import ErrorMessage from './_components/ErrorMessage'
+import CommaNumberField from './_components/CommaNumberField'
+import OptionSelection from './_components/OptionSelection/OptionSelection'
+import MultipleOptionSelection from './_components/OptionSelection/MultipleOptionSelection'
 
 export {
   DatePickerComponent,
+  InputField,
+  TextComponent,
+  ErrorMessage,
+  CommaNumberField,
+  PopoverComponent,
   DateRangePickerComponent,
   CheckboxComponent,
   OptionSelectionComponent,
+  OptionSelection,
+  MultipleOptionSelection,
   RadioButtonsComponent,
   MultipleOptionSelectionComponent,
   FormWrapper,
@@ -23,5 +37,5 @@ export {
   uppercasefy,
   formatDate,
   titleCasify,
-  parseInitialValue
+  parseInitialValue,
 }
